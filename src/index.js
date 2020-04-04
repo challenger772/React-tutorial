26c43b395f45d46ec6829cec296a07c46f6cffbb
challenger772 <chalenger772@outlook.com>
2020-04-05 02:06:45 +0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <img  src={this.props.author.avatarUrl} 
+            alt={this.props.author.name}
+          />
+          <div>{this.props.author.name}</div>
+        </div>
+        <div>{this.props.text}</div>
+        <div>{formatDate(this.props.date)}</div>
+      </div>
+    );
+  }
+}
+
+function formatDate(date) {
+  return date.toLocaleTimeString();
+}
+
+const comment = {
+  text: 'this is my page',
+  author: {
+    avatarUrl: 'https://placekitten.com/g/64/64',
+    name: 'John Smith'
+  },
+  date: new Date()
+}
+
+ReactDom.render(
+  <App 
+    text={comment.text}
+    author={comment.author}
+    date={comment.date}
+  />,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
